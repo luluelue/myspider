4d7752aad96f7e5980e8735fd3a77c6a239c7bd1
duoyi <1132923539@qq.com>
2020-06-15 10:24:01 +0800

    获取GitHub登录cookie

--- myspider/myspider/spiders/github.py
+++ myspider/myspider/spiders/github.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+
 import scrapy
 
 
@@ -18,14 +20,34 @@ class GithubSpider(scrapy.Spider):
     # required_field_402d:
     # timestamp: 1592161010257
     # timestamp_secret: f2d2d798c7410983b7896fc42def973360109045bbb3834328238e950d536b51
-    def parse(self, response):
-        authenticity_token = response.xpath("//input[@name='authenticity_token']/@value()").extract_first()
-        ga_id = response.xpath("//input[@name='ga_id']/@value()").extract_first()
-        commit = response.xpath("//input[@name='commit']/@value()").extract_first()
-        timestamp = response.xpath("//input[@name='timestamp']/@value()").extract_first()
-        timestamp_secret = response.xpath("//input[@name='timestamp_secret']/@value()").extract_first()
-        post_data = dict(
-            login = "1132923539@qq.com",
-            password = "",
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name=None, **kwargs)
+        self.pwd = None
 
+    def parse(self, response):
+        authenticity_token = response.xpath("//input[@name='authenticity_token']/@value").extract_first()
+        ga_id = response.xpath("//input[@name='ga_id']/@value").extract_first()
+        commit = response.xpath("//input[@name='commit']/@value").extract_first()
+        timestamp = response.xpath("//input[@name='timestamp']/@value").extract_first()
+        timestamp_secret = response.xpath("//input[@name='timestamp_secret']/@value").extract_first()
+        # utf8 = response.xpath("//input[@name='utf8']/@value").extract_first()
+        post_data = dict(
+            login="1132923539@qq.com",
+            password=self.pwd,
+            commit=commit,
+            authenticity_token=authenticity_token,
+            # ga_id=ga_id,
+            # webauthn-support = "s",
+            timestamp=str(timestamp),
+            timestamp_secret=str(timestamp_secret),
+            # utf8="utf8"
         )
+        yield scrapy.FormRequest(
+            url="https://github.com/session",
+            formdata=post_data,
+            method='POST',
+            callback=self.parse_index
+        )
+
+    def parse_index(self, response):
+        print(re.findall("lulu", response.body.decode()))
